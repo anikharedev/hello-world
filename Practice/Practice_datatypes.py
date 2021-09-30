2f86516fd29e8103586c074acf9af7d4f352ba13
anikharedev <anikharedev@gmail.com>
2021-09-30 15:50:28 -0400

    Test Commit

--- Practice/Practice_datatypes.py
+++ Practice/Practice_datatypes.py
@@ -6,3 +6,16 @@
 my_city = "New York"
 
 print ("I live in", my_city)
+
+current_floor = 6
+destination_floor = 6
+
+if (current_floor < destination_floor):
+    print ("You need to go up")
+elif (current_floor > destination_floor):
+    print ("You need to go down")
+elif (current_floor == destination_floor):
+    print ("You are at the destination")
+
+
+
