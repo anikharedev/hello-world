687bf537305e56681a2595f3f0141b331077a3c7
anikharedev <anikharedev@gmail.com>
2021-09-30 15:47:09 -0400

    Test Commit

---
+++ Practice/Practice_datatypes.py
@@ -0,0 +1,8 @@
+##
+## Practice Data Types
+##
+
+## Whenever we want to use data, we declare and assign it to a "variable", in this case, my_city
+my_city = "New York"
+
+print ("I live in", my_city)
